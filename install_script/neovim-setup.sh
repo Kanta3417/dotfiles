d6f813e3242134eb6ee82360c331b94e411eddcd
taraba3417 <uesugi.kanta912@mail.kyutech.jp>
2024-04-26 14:56:53 +0900

    exec install.sh in neovim-setup.sh

--- install_script/neovim-setup.sh
+++ install_script/neovim-setup.sh
@@ -4,11 +4,29 @@
 # -e: 実行したコマンドが1つでもエラーになったら直ちにシェルを終了
 set -ue
 
+dest=$HOME/.local
+bashrc=$HOME/.bashrc
+nvim_plugin_path=$dest/nvim-linux64/share/nvim/runtime/plugin/matchit.vim
+
+helpmsg() {
+  echo "使用できるオプション: $0 [--help | -h]" 0>&2
+  echo "デバッグモードでシェルを実行: $0 [--debug | -d]" 0>&2
+  # echo "アンインストール: $0 [--uninstall]" 0>&2
+}
+
 while [ $# -gt 0 ];do
   case ${1} in
     --debug|-d)
       # -x: シェルが実行したコマンドと引数を出力
       set -uex
+      ;;
+    --help|-h)
+      helpmsg
+      exit 1
+      ;;
+    --uninstall)
+      uninstall
+      exit 1
       ;;
     *)
       ;;
@@ -16,32 +34,32 @@ while [ $# -gt 0 ];do
   shift
 done
 
-dest=$HOME/.local
-bashrc=$HOME/.bashrc
-nvim_plugin_path=$dest/nvim-linux64/share/nvim/runtime/plugin/matchit.vim
-
 # $destがあるか確認
 if [[ ! -d $dest ]]; then
-  echo "$dest not found... Auto make."
+  echo -e "\e[1;32m $dest not found... Auto make.\e[m"
   mkdir -p $dest
 fi
 
 # Neovimの実行ファイルをダウンロード
-echo "Download Neovim"
+echo -e "\e[1;32m  Download Neovim \e[m"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 tar -C $dest -xzf nvim-linux64.tar.gz
 
 # パスを通す
-echo "Create path"
+echo -e "\e[1;32m  Create path \e[m"
 echo -e "\n# Neovim" >> $bashrc
 echo "export PATH=\"$dest/nvim-linux64/bin:\$PATH\"" >> $bashrc
 
 # %による括弧ジャンプが使えるように変更
-echo "Modify $nvim_plugin_path"
+echo -e "\e[1;32m  Modify $nvim_plugin_path \e[m"
 sed -i 's/!exists("g:loaded_matchit") && //' $nvim_plugin_path
 
 # 設定ファイルのインストール
-echo "Install dotfiles"
+echo -e "\e[1;32m Install dotfiles \e[m"
 git clone https://github.com/Kanta3417/dotfiles.git
 cd dotfiles
-install.sh
+bash install.sh
+
+echo -e "\e[1;36m setup success!!! \e[m"
+
+
